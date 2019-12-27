8e77a2c106a82b0b0a7e57ee6cde6df7b50b8a77
cliffbb <bronibbc@yahoo.co.uk>
2019-12-28 00:23:43 +0900

    NAS pipeline completed

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import  setup
 setup(
         name='geppy_nn',
         version='0.1dev',
-        packages=['geppy_nn']
+        packages=['geppy_nn'],
+        install_requires=['deap', 'geppy']
 )
